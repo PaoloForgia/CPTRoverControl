93edb4ca6d5a3d9c753ce406d2f64c660bff0a25
PaoloForgia <paolo.forgia@gmail.com>
2022-10-15 13:41:44 +0200

    Aggiunti Unit test funzionanti

--- RoverControlApp/RoverControlApp/RoverControlApp/Services/Commands.cs
+++ RoverControlApp/RoverControlApp/RoverControlApp/Services/Commands.cs
@@ -8,75 +8,61 @@ namespace RoverControlApp.Services
 {
     public class Commands
     {
-        public string EngineLeft(int value)
+        public static string EngineLeft(int value)
         {
             if (!IsSpeedValid(value)) return null;
 
             return ToCommand("L", value);
         }
 
-        public string EngineRight(int value)
+        public static string EngineRight(int value)
         {
             if (!IsSpeedValid(value)) return null;
 
             return ToCommand("R", value);
         }
 
-        public string Buzzer(int value)
+        public static string Buzzer(bool value)
         {
-            if (!IsBoolValid(value)) return null;
-
-            return ToCommand("B", value);
-
+            return ToCommand("B", ToNumber(value));
         }
 
-        public string LightFront(int value)
+        public static string LightFront(bool value)
         {
-            if (!IsBoolValid(value)) return null;
-
-            return ToCommand("F", value);
-
+            return ToCommand("F", ToNumber(value));
         }
 
-        public string LightBack(int value)
+        public static string LightBack(bool value)
         {
-            if (!IsBoolValid(value)) return null;
-
-            return ToCommand("P", value);
-
+            return ToCommand("P", ToNumber(value));
         }
 
-        public string EmergencyStop(int value)
+        public static string EmergencyStop(bool value)
         {
-            if (!IsBoolValid(value)) return null;
-
-            return ToCommand("S", value);
-
+            return ToCommand("S", ToNumber(value));
         }
 
-        public Data Translate(string value)
+        public static Data Translate(string value)
         {
-            if (value == null || !IsValidData(value)) return null;
+            if (!IsValidData(value)) return null;
 
             var type = value[0];
-            if (type.Equals("T")) return new Data() { Name = "Batteria", Value = GetValue(value) };
-            else if (type.Equals("D")) return new Data() { Name = "Distanza", Value = GetValue(value) };
+            if (type.Equals('T')) return new Data() { Name = "Batteria", Value = GetValue(value) };
+            else if (type.Equals('D')) return new Data() { Name = "Distanza", Value = GetValue(value) };
             else return null;
         }
 
         public static bool IsMultipleValue(string value) => value.Split('\n').Length > 1;
 
-        private int? GetValue(string value)
+        private static int? GetValue(string value)
         {
-            var numericValue = value.Substring(1, 3);
+            var numericValue = value.Substring(1, value.Length - 2); // Last two letters are \n
             return int.Parse(numericValue);
         }
 
         public static string ToCommand(string command, int value) => $"{command}{value}\n";
 
-        public static bool IsSpeedValid(int value) => value < 0 || value > 255;
-
-        public static bool IsBoolValid(int value) => value == 0 || value == 1;
+        public static bool IsSpeedValid(int value) => value >= 0 && value <= 255;
 
         public static bool IsValidData(string value)
         {
@@ -109,6 +95,11 @@ namespace RoverControlApp.Services
                 default:
                     return -1;
             }
+        }
+
+        private static int ToNumber(bool value)
+        {
+            return value ? 1 : 0;
         }
     }
 }
---
+++ RoverControlApp/Tests/CommandsTest.cs
@@ -0,0 +1,217 @@
+using NUnit.Framework;
+using RoverControlApp.Models;
+using RoverControlApp.Services;
+
+namespace Tests
+{
+    public class Tests
+    {
+
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Buzzer_IsOn()
+        {
+            var result = Commands.Buzzer(true);
+
+            Assert.That(result, Is.EqualTo("B1\n"));
+        }
+
+        [Test]
+        public void Buzzer_IsOff()
+        {
+            var result = Commands.Buzzer(false);
+
+            Assert.That(result, Is.EqualTo("B0\n"));
+        }
+
+        [Test]
+        public void LightFront_IsOn()
+        {
+            var result = Commands.LightFront(true);
+
+            Assert.That(result, Is.EqualTo("F1\n"));
+        }
+
+        [Test]
+        public void LightFront_IsOff()
+        {
+            var result = Commands.LightFront(false);
+
+            Assert.That(result, Is.EqualTo("F0\n"));
+        }
+
+        [Test]
+        public void LightBack_IsOn()
+        {
+            var result = Commands.LightBack(true);
+
+            Assert.That(result, Is.EqualTo("P1\n"));
+        }
+
+        [Test]
+        public void LightBack_IsOff()
+        {
+            var result = Commands.LightBack(false);
+
+            Assert.That(result, Is.EqualTo("P0\n"));
+        }
+
+        [Test]
+        public void EmergencyStop_IsOn()
+        {
+            var result = Commands.EmergencyStop(true);
+
+            Assert.That(result, Is.EqualTo("S1\n"));
+        }
+
+        [Test]
+        public void EmergencyStop_IsOff()
+        {
+            var result = Commands.EmergencyStop(false);
+
+            Assert.That(result, Is.EqualTo("S0\n"));
+        }
+
+        [TestCase(0)]
+        [TestCase(32)]
+        [TestCase(128)]
+        [TestCase(196)]
+        [TestCase(255)]
+        public void EngineLeft_ValidValues(int value)
+        {
+            var result = Commands.EngineLeft(value);
+
+            Assert.That(result, Is.EqualTo($"L{value}\n"));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-128)]
+        [TestCase(256)]
+        public void EngineLeft_InvalidValues(int value)
+        {
+            var result = Commands.EngineLeft(value);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [TestCase(0)]
+        [TestCase(32)]
+        [TestCase(128)]
+        [TestCase(196)]
+        [TestCase(255)]
+        public void EngineRight_ValidValues(int value)
+        {
+            var result = Commands.EngineRight(value);
+
+            Assert.That(result, Is.EqualTo($"R{value}\n"));
+        }
+
+        [TestCase(-1)]
+        [TestCase(-128)]
+        [TestCase(256)]
+        public void EngineRight_InvalidValues(int value)
+        {
+            var result = Commands.EngineRight(value);
+
+            Assert.That(result, Is.Null);
+        }
+
+        [TestCase("L0\n")]
+        [TestCase("L128\n")]
+        [TestCase("L255\n")]
+        [TestCase("R0\n")]
+        [TestCase("R128\n")]
+        [TestCase("R255\n")]
+        [TestCase("B0\n")]
+        [TestCase("B1\n")]
+        [TestCase("F0\n")]
+        [TestCase("F1\n")]
+        [TestCase("P0\n")]
+        [TestCase("P1\n")]
+        [TestCase("S0\n")]
+        [TestCase("S1\n")]
+        [TestCase("T0\n")]
+        [TestCase("T50\n")]
+        [TestCase("T100\n")]
+        [TestCase("D0\n")]
+        [TestCase("D50\n")]
+        [TestCase("D100\n")]
+        public void IsValidData_IsTrue(string data)
+        {
+            Assert.That(Commands.IsValidData(data), Is.True);
+        }
+
+
+        [TestCase("L0")]
+        [TestCase("L-1\n")]
+        [TestCase("L256\n")]
+        [TestCase("L\n")]
+        [TestCase("R-1\n")]
+        [TestCase("R256\n")]
+        [TestCase("R\n")]
+        [TestCase("B-1\n")]
+        [TestCase("B2\n")]
+        [TestCase("B\n")]
+        [TestCase("F-1\n")]
+        [TestCase("F2\n")]
+        [TestCase("F\n")]
+        [TestCase("P-1\n")]
+        [TestCase("P2\n")]
+        [TestCase("P\n")]
+        [TestCase("S-1\n")]
+        [TestCase("S2\n")]
+        [TestCase("S\n")]
+        [TestCase("T-1\n")]
+        [TestCase("T101\n")]
+        [TestCase("T\n")]
+        [TestCase("D-1\n")]
+        [TestCase("D101\n")]
+        [TestCase("D\n")]
+        public void IsValidData_IsFalse(string data)
+        {
+            Assert.That(Commands.IsValidData(data), Is.False);
+        }
+
+        [TestCase("T0\n", 0)]
+        [TestCase("T50\n", 50)]
+        [TestCase("T100\n", 100)]
+        public void Translate_IsBattery(string command, int value)
+        {
+            var data = Commands.Translate(command);
+
+            Assert.That(data, Is.Not.Null);
+            Assert.That(data.Name, Is.EqualTo("Batteria"));
+            Assert.That(data.Value, Is.EqualTo(value));
+        }
+
+        [TestCase("D0\n", 0)]
+        [TestCase("D50\n", 50)]
+        [TestCase("D100\n", 100)]
+        public void Translate_IsDistance(string command, int value)
+        {
+            var data = Commands.Translate(command);
+
+            Assert.That(data, Is.Not.Null);
+            Assert.That(data.Name, Is.EqualTo("Distanza"));
+            Assert.That(data.Value, Is.EqualTo(value));
+        }
+
+        [TestCase("T\n")]
+        [TestCase("T-1\n")]
+        [TestCase("T101\n")]
+        [TestCase("D\n")]
+        [TestCase("D-1\n")]
+        [TestCase("D101\n")]
+        [TestCase("A0\n")]
+        public void Translate_IsInvalidData(string command)
+        {
+            var data = Commands.Translate(command);
+
+            Assert.That(data, Is.Null);
+        }
+    }
+}
